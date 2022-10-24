0d346a6a01f4923ba1e6ada714221c46d622d1f7
Kevin <kuhes0918@gmail.com>
2022-10-24 11:11:06 -0500

    estilos y formato

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 const urlBase = 'https://rickandmortyapi.com/api/character';
 
+const loadData = (urlBase) => {
 fetch(urlBase)
 .then(respuesta => {
      return respuesta.json();
-}) .then(respJson => {
-    console.log(respJson);
+}) 
+.then(respJson => {
     const info = respJson.info;
     const personajes = respJson.results;
     console.log(info);
     console.log(personajes);
     showCharacters(personajes);
 })
-
+}
 const showCharacters = (personajes) => {
-    const listapersonajes = document.querySelector('ul');
+    const listaPersonajes = document.querySelector('#characters');
     personajes.forEach(personaje => {
-        const li = document.createElement('li');
-        li.innerText = `Personaje ${personaje.name}`;
-        listapersonajes.appendChild(li);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.classList.add('m-3');
+        div.innerHTML = creaCard(personaje); 
+        listaPersonajes.appendChild(div);
     })
 }
+
+const creaCard = (personaje) => {
+    const html =`
+      <div class="card" style="width: 18rem;">
+         <img src="${personaje.image}" class="card-img-top" alt="${personaje.image}">
+         <div class="card-body">
+             <h5 class="card-title">${personaje.name}</h5>
+             <p class="card-text">Is ${personaje.status}</p>
+             <a href="#" class="btn btn-primary">Ver más</a>
+            </div>
+            </div>
+        `;
+        return html;
+}
+
+loadData(urlBase);
